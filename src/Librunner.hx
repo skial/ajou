7931e1af21428730c6baa8631074dc1cb0c977eb
Skial Bainn <skial.bainn@gmail.com>
2014-07-04 14:37:09 +0100

    Flesh out Librunner.hx Simplify Ajou.hx

--- src/Librunner.hx
+++ src/Librunner.hx
@@ -1,7 +1,13 @@
 package ;
 
 import neko.Lib;
+import sys.io.File;
 import uhx.sys.Ajou;
+
+using Sys;
+using sys.io.File;
+using haxe.io.Path;
+using sys.FileSystem;
 
 /**
  * ...
@@ -13,11 +19,102 @@ import uhx.sys.Ajou;
 class Librunner implements Klas {
 	
 	static function main() {
-		new Librunner( Sys.args() );
+		var ajou = new Librunner( Sys.args() );
 	}
 	
+	/**
+	 * Allows you to run `ajou [options]` from now on.
+	 */
+	@alias('g') 
+	public var global:Bool;
+	
+	/**
+	 * Compress the output.
+	 */
+	@alias('c') 
+	public var compress:Bool;
+	
+	/**
+	 * The css file you want to process.
+	 */
+	@alias('i')
+	public var input:String;
+	
+	/**
+	 * The location you want the processed file to be saved to.
+	 */
+	@alias('o')
+	public var output:String;
+	
+	/**
+	 * A list of plugin names to use with Ajou.
+	 */
+	@alias('p')
+	public var plugins:Array<String>;
+	
+	private var directory:String;
+	
 	public function new(args:Array<String>) {
-		new Ajou('');
+		
+		
+		trace( args );
+		trace( input );
+		trace( output );
+		trace( plugins );
+		
+		if (input == null || input == '') {
+			'You are missing an input file.'.println();
+			return;
+		}
+		
+		if (output == null || output == '') {
+			'You seem to have forgotten to specify an output location.'.println();
+			return;
+		}
+		
+		directory = args[args.length - 1].normalize();
+		directory.setCwd();
+		
+		if (global) makeGlobal();
+		
+		var ajou = new Ajou( '$directory/$input'.normalize().getContent() );
+		for (plugin in plugins) ajou.use( plugin );
+		ajou.rework();
+		File.saveContent( '$directory/$output'.normalize(), ajou.toString( compress ) );
+	}
+	
+	private function makeGlobal() {
+		if (!Sys.environment().exists( 'HAXEPATH' )) {
+			'The enviroment HAXEPATH does not exist.'.println();
+			return;
+		}
+		
+		var path = Sys.environment().get( 'HAXEPATH' ).normalize();
+		
+		switch (Sys.systemName().toLowerCase()) {
+			case _.indexOf( 'windows' ) > -1 => true if ('$path/ajou.bat'.normalize().exists()):
+				'Ajou is already global.'.println();
+				return;
+				
+			case _ if ('$path/ajou.sh'.normalize().exists()): 
+				'Ajou is already global.'.println();
+				return;
+				
+		}
+		
+		switch (Sys.systemName().toLowerCase()) {
+			case _.indexOf( 'windows' ) > -1 => true:
+				File.saveContent( '$path/ajou.bat'.normalize(), '@echo off\r\nhaxelib run ajou %*' );
+				
+			case _.indexOf( 'linux' ) > -1 => true:
+				File.saveContent( '$path/ajou.sh'.normalize(), '# Bash\r\n#!/bin/sh\r\nhaxelib run ajou $@' );
+				
+			case _.indexOf( 'mac' ) > -1 => true:
+				File.saveContent( '$path/ajou.sh'.normalize(), '# Bash\r\n#!/bin/sh\r\nhaxelib run ajou $@' );
+				
+			case _: 
+				'Can not determine the OS type, apologies.'.println();
+		}
 	}
 	
 }
--- src/uhx/sys/Ajou.hx
+++ src/uhx/sys/Ajou.hx
@@ -4,11 +4,10 @@ import uhx.mo.Token;
 import byte.ByteData;
 import uhx.lexer.CssLexer;
 import uhx.lexer.CssParser;
+import uhx.Tappi;
 
 typedef AjouPlugin = {
-	@:optional public function onMedia(token:CssMedia, ajou:Ajou):CssMedia;
-	@:optional public function onSelector(token:CssSelectors, ajou:Ajou):CssSelectors;
-	@:optional public function onDeclaration(token:CssKeywords, ajou:Ajou):CssKeywords;
+	public function handler(tokens:Array<Token<CssKeywords>>, ajou:Ajou):Array<Token<CssKeywords>>;
 }
 
 /**
@@ -21,61 +20,46 @@ class Ajou {
 	private var parser:CssParser;
 	private var tokens:Array<Token<CssKeywords>>;
 	
-	public var media:Array<CssMedia->Ajou->CssMedia>;
-	public var blocks:Array<CssKeywords->Ajou->CssKeywords>;
-	public var selectors:Array<CssSelectors->Ajou->CssSelectors>;
+	private var callbacks:Array<Array<Token<CssKeywords>>->Ajou->Array<Token<CssKeywords>>>;
+	
+	#if tappi
+	public var tappi:Tappi;
+	#end
 	
 	public function new(css:String) {
-		media = [];
-		blocks = [];
-		selectors = [];
+		callbacks = [];
 		
 		parser = new CssParser();
 		tokens = parser.toTokens( ByteData.ofString( css ), 'ajou' );
 		
-		Tappi.haxelib = true;
+		#if tappi
+		tappi = new Tappi([], true);
+		#end
 	}
 	
 	public function use(plugin:String):Void {
-		Tappi.libraries.push( plugin );
+		#if tappi
+		tappi.libraries.push( plugin );
+		#end
 	}
 	
 	public function rework():Void {
 		#if tappi
-		Tappi.load();
+		tappi.find();
+		tappi.load();
 		
-		for (id in Tappi.classes.keys()) {
-			var cls:AjouPlugin = std.Type.createInstance( Tappi.classes.get( id ), [] );
-			if (cls.onMedia != null) media.push( cls.onMedia );
-			if (cls.onSelector != null) selectors.push( cls.onSelector );
-			if (cls.onDeclaration != null) blocks.push( cls.onDeclaration );
+		for (id in tappi.classes.keys()) {
+			var cls:AjouPlugin = std.Type.createInstance( tappi.classes.get( id ), [] );
+			callbacks.push( cls.handler );
 		}
 		#end
 		
-		tokens = tokens.map( function(t) return switch (t.token) {
-			case Keyword(x): 
-				switch (x) {
-					case RuleSet(_, _):
-						for (cb in selectors) x = cb(x, this);
-						
-					case AtRule(_, _, _):
-						for (cb in media) x = cb(x, this);
-						
-					case Declaration(_, _):
-						for (cb in blocks) x = cb(x, this);
-						
-					case _:
-				}
-				x;
-				
-			case _:
-				x;
-				
-		} );
+		for (cb in callbacks) tokens = cb(tokens, this);
+		
 	}
 	
 	public function toString(compress:Bool = false, sourceMap:Bool = false):String {
-		return [for (token in tokens) parser.printString( token, compress )].join( compress ? '' : '\n' );
+		return [for (token in tokens) parser.printString( token, compress )].join( compress ? '' : '\r\n' );
 	}
 	
 }
